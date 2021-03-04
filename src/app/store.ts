45de6ec713acc711f5a0de23287a838c4cf19199
leularia <leulsegedtariku@gmail.com>
2021-03-04 21:46:49 +0300

    ✨ NEW FEATURE: init redux Store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import editorReducer from '../features/editor';
+import productReducer from '../features/product';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    editor: editorReducer,
+    product: productReducer
   },
 });
 
