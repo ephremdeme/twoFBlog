539d5f21aaafced71f741319d325a6f6c8273c05
Ephrem Demelash <demelashephrem@gmail.com>
2021-03-05 11:38:37 +0300

    📦 NEW: adds text formating settings

--- src/components/user/RenderNode.tsx
+++ src/components/user/RenderNode.tsx
@@ -1,134 +1,140 @@
-import React, { useEffect, useRef, useCallback } from "react";
-import { useNode, useEditor } from "@craftjs/core";
-import ReactDOM from "react-dom";
-import { ROOT_NODE } from "@craftjs/utils";
-import { ArrowUpward, Delete, MoveToInbox } from "@material-ui/icons";
-import "./RenderNode.css";
+import React, {useEffect, useRef, useCallback} from 'react';
+import {useNode, useEditor} from '@craftjs/core';
+import ReactDOM from 'react-dom';
+import {ROOT_NODE} from '@craftjs/utils';
+import {ArrowUpward, Delete, MoveToInbox} from '@material-ui/icons';
+import './RenderNode.css';
 
-const RenderNode = ({ render }: any) => {
-  const { actions, query, connectors } = useEditor();
+const RenderNode = ({render}: any) => {
+	const {actions, query, selected, connectors} = useEditor((state, query) => {
+		let selected;
 
-  const {
-    id,
-    isActive,
-    isHover,
-    dom,
-    name,
-    moveable,
-    deletable,
-    connectors: { drag },
-    parent,
-  } = useNode((node) => ({
-    isActive: node.events.selected,
-    isHover: node.events.hovered,
-    dom: node.dom,
-    name: node.data.custom.displayName || node.data.displayName,
-    moveable: query.node(node.id).isDraggable(),
-    deletable: query.node(node.id).isDeletable(),
-    parent: node.data.parent,
-    props: node.data.props,
-  }));
+		const currentNodeId = state.events.selected;
 
-  const currentRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
+		if (currentNodeId) {
+			selected = {
+				id: currentNodeId,
+				name: state.nodes[currentNodeId].data.name,
+				settings:
+					state.nodes[currentNodeId].related &&
+					state.nodes[currentNodeId].related.settings,
+				isDeletable: query.node(currentNodeId).isDeletable(),
+			};
+		}
 
-  useEffect(() => {
-    if (dom) {
-      if (isActive && isHover) dom.classList.add("component-selected");
-      else dom.classList.remove("component-selected");
-    }
-  }, [dom, isActive, isHover]);
+		return {
+			selected,
+		};
+	});
 
-  const getPos = useCallback((dom: HTMLElement | null) => {
-    const { top, left, bottom } = dom
-      ? dom.getBoundingClientRect()
-      : { top: 0, left: 0, bottom: 0 };
-    return {
-      top: `${top > 0 ? top : bottom}px`,
-      left: `${left}px`,
-    };
-  }, []);
+	const {
+		id,
+		isActive,
+		isHover,
+		dom,
+		name,
+		moveable,
+		deletable,
+		connectors: {drag},
+		parent,
+	} = useNode((node) => ({
+		isActive: node.events.selected,
+		isHover: node.events.hovered,
+		dom: node.dom,
+		name: node.data.custom.displayName || node.data.displayName,
+		moveable: query.node(node.id).isDraggable(),
+		deletable: query.node(node.id).isDeletable(),
+		parent: node.data.parent,
+		props: node.data.props,
+	}));
 
-  const scroll = useCallback(() => {
-    const { current: currentDOM } = currentRef;
+	const currentRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLDivElement>;
 
-    if (!currentDOM) return;
-    const { top, left } = getPos(dom);
-    currentDOM.style.top = top;
-    currentDOM.style.left = left;
-  }, [dom]);
+	useEffect(() => {
+		if (dom) {
+			if (isActive || isHover) dom.classList.add('component-selected');
+			else dom.classList.remove('component-selected');
+		}
+	}, [dom, isActive, isHover]);
 
-  useEffect(() => {
-    const renderer = document.querySelector(".craftjs-renderer");
-    if (renderer) renderer.addEventListener("scroll", scroll);
+	const getPos = useCallback((dom: HTMLElement | null) => {
+		const {top, left, bottom} = dom
+			? dom.getBoundingClientRect()
+			: {top: 0, left: 0, bottom: 0};
+		return {
+			top: `${top > 0 ? top : bottom}px`,
+			left: `${left}px`,
+		};
+	}, []);
 
-    return () => {
-      if (renderer) renderer.removeEventListener("scroll", scroll);
-    };
-  }, [scroll]);
+	const scroll = useCallback(() => {
+		const {current: currentDOM} = currentRef;
 
-  return (
-    <>
-      {isActive && isHover
-        ? ReactDOM.createPortal(
-            <div
-              ref={currentRef}
-              className="indicator"
-              style={{
-                left: getPos(dom).left,
-                top: getPos(dom).top,
-                zIndex: 9999,
-              }}
-            >
-              <h2 className="flex-1 mr-4">{name}</h2>
-              {moveable ? (
-                <button className="btn btn-move" ref={drag}>
-                  <MoveToInbox />
-                </button>
-              ) : null}
-              {id !== ROOT_NODE && (
-                <button
-                  className="btn btn-cursor"
-                  onClick={() => {
-                    actions.selectNode(parent);
-                  }}
-                >
-                  <ArrowUpward />
-                </button>
-              )}
+		if (!currentDOM) return;
+		const {top, left} = getPos(dom);
+		currentDOM.style.top = top;
+		currentDOM.style.left = left;
+	}, [dom]);
 
-              {name === "Blockquote" && (
-                <button
-                  className="btn btn-cursor"
-                  onClick={() => {
-                    actions.setProp(id, (props) => {
-                      if (props.bold === "bold" || props.bold === "bolder")
-                        props.bold = "normal";
-                      else props.bold = "bolder";
-                    });
-                  }}
-                >
-                  BB
-                </button>
-              )}
+	useEffect(() => {
+		const renderer = document.querySelector('.craftjs-renderer');
+		if (renderer) renderer.addEventListener('scroll', scroll);
 
-              {deletable ? (
-                <button
-                  className="btn-cursor"
-                  onMouseDown={(e: React.MouseEvent) => {
-                    e.stopPropagation();
-                    actions.delete(id);
-                  }}
-                >
-                  <Delete />
-                </button>
-              ) : null}
-            </div>,
-            document.body
-          )
-        : null}
-      {render}
-    </>
-  );
+		return () => {
+			if (renderer) renderer.removeEventListener('scroll', scroll);
+		};
+	}, [scroll]);
+
+	return (
+		<>
+			{isActive || isHover
+				? ReactDOM.createPortal(
+						<div
+							ref={currentRef}
+							className="indicator"
+							style={{
+								left: getPos(dom).left,
+								top: getPos(dom).top,
+								zIndex: 9999,
+							}}>
+							<h2 className="flex-1 mr-4">{name}</h2>
+							{moveable ? (
+								<button className="btn btn-move" ref={drag}>
+									<MoveToInbox />
+								</button>
+							) : null}
+							{id !== ROOT_NODE && (
+								<button
+									className="btn btn-cursor"
+									onClick={() => {
+										actions.selectNode(parent);
+									}}>
+									<ArrowUpward />
+								</button>
+							)}
+
+							{name === 'Blockquote' &&
+								selected &&
+								selected.settings &&
+								React.createElement(selected.settings)}
+
+							{deletable ? (
+								<button
+									className="btn-cursor"
+									onMouseDown={(e: React.MouseEvent) => {
+										e.stopPropagation();
+										actions.delete(id);
+									}}>
+									<Delete />
+								</button>
+							) : null}
+						</div>,
+						document.body
+				  )
+				: null}
+			{render}
+		</>
+	);
 };
 
 export default RenderNode;
--- src/components/user/blockqoute.tsx
+++ src/components/user/blockqoute.tsx
@@ -1,125 +1,134 @@
-import React, { useEffect, useRef, useState } from "react";
-import PropTypes from "prop-types";
-import { Button, makeStyles, Popper } from "@material-ui/core";
-import { useNode, UserComponent } from "@craftjs/core";
-import ContentEditable, { ContentEditableEvent } from "react-contenteditable";
-
+import React, {useEffect, useRef, useState} from 'react';
+import PropTypes, {ReactComponentLike} from 'prop-types';
+import {Button, makeStyles, Popper} from '@material-ui/core';
+import {useNode, UserComponent} from '@craftjs/core';
+import ContentEditable, {ContentEditableEvent} from 'react-contenteditable';
+import {ReactComponent as Bold} from '../../public/svg/bold.svg';
+import {ReactComponent as Italic} from '../../public/svg/italic.svg';
+import {ReactComponent as Link} from '../../public/svg/link.svg';
+import {ReactComponent as Quote} from '../../public/svg/quote.svg';
+import {ReactComponent as OrderedList} from '../../public/svg/number.svg';
+import {ReactComponent as UnorderedList} from '../../public/svg/bulletlist.svg';
+import {ReactComponent as Underline} from '../../public/svg/underline.svg';
 
 const useStyles = makeStyles({
-  root: {
-  },
-  blockqoute: {
-    display: "block",
-    borderLeftStyle: "solid",
-    borderLeftColor: "#1279BE",
-    minHeight: "70px",
-  },
+	root: {},
+	blockqoute: {
+		display: 'block',
+		borderLeftStyle: 'solid',
+		borderLeftColor: '#1279BE',
+		minHeight: '70px',
+	},
 });
 
 export const Blockqoute: UserComponent<BlockqouteProps> = ({
-  text,
-  textAlign,
-  fontSize,
+	text,
+	textAlign,
+	fontSize,
 }) => {
-  const classes = useStyles();
-  const html = useRef(text || "Edit Blockqoute");
-  const inputRef = useRef<HTMLLinkElement>(null);
+	const classes = useStyles();
+	const html = useRef(text || 'Edit Blockqoute');
+	const inputRef = useRef<HTMLLinkElement>(null);
 
-  const {
-    connectors: { connect, drag },
-    selected,
-    actions: { setProp },
-  } = useNode((state) => ({
-    selected: state.events.selected,
-    dragged: state.events.dragged,
-  }));
+	const {
+		connectors: {connect, drag},
+		selected,
+		actions: {setProp},
+	} = useNode((state) => ({
+		selected: state.events.selected,
+		dragged: state.events.dragged,
+	}));
 
-  const [editable, setEditable] = useState(false);
+	const [editable, setEditable] = useState(false);
 
-  const [anchorEl, setAnchorEl] = React.useState(null);
+	useEffect(() => {
+		if (selected) {
+			return;
+		}
 
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
+		setEditable(false);
+	}, [selected]);
 
-  const handleClick = (event: any) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-    if (anchorEl) inputRef.current && inputRef.current.focus();
-  };
-
-  useEffect(() => {
-    if (selected) {
-      return;
-    }
-
-    setEditable(false);
-  }, [selected]);
-
-  const handleChange = (e: ContentEditableEvent) => {
-    html.current = e.target.value;
-  };
-  return (
-    <div
-      className={classes.root}
-      onClick={(e) => selected && setEditable(true)}
-      ref={(ref) => connect(drag(ref))}
-    >
-      <ContentEditable
-        innerRef={inputRef}
-        html={html.current}
-        onChange={handleChange}
-        disabled={!editable}
-        aria-describedby={id}
-        onFocus={handleClick}
-        tagName={"blockquote"}
-        className={classes.blockqoute}
-        style={{ fontSize: `${fontSize}px`, textAlign }}
-        title="Editable"
-      />
-
-      <Popper id={id} open={open} anchorEl={anchorEl} placement={"top"}>
-        <div style={{ display: "inline-block" }}>
-          
-          <EditButton cmd="bold" name={"B"} />
-          <EditButton cmd="italic" name={"I"} />
-          <EditButton cmd="underline" name={"U"} />
-          <EditButton cmd="justifyleft" name={"AL"} />
-          <EditButton cmd="justifycenter" name={"AC"} />
-          <EditButton cmd="justifyright" name={"AR"} />
-          <EditButton cmd="insertorderedlist" name={"Ol"} />
-          <EditButton cmd="insertunorderedlist" name={"UL"} />
-          <EditButton cmd="formatblock" value="blockquote" name={"BQ"} />
-          <EditButton cmd="createlink" name={"Link"} />
-        </div>
-      </Popper>
-    </div>
-  );
+	const handleChange = (e: ContentEditableEvent) => {
+		html.current = e.target.value;
+	};
+	return (
+		<div
+			className={classes.root}
+			onClick={(e) => selected && setEditable(true)}
+			ref={(ref) => connect(drag(ref))}>
+			<ContentEditable
+				innerRef={inputRef}
+				html={html.current}
+				onChange={handleChange}
+				disabled={!editable}
+				tagName={'blockquote'}
+				className={classes.blockqoute}
+				// style={{fontSize: `${fontSize}px`, textAlign}}
+				title="Editable"
+			/>
+		</div>
+	);
 };
 
-const EditButton: React.FC<{ name: string; cmd: string; value?: string }> = (props) => {
-  return (
-    <Button
-      key={props.cmd}
-      style={{ margin: "8px" }}
-      onMouseDown={(evt) => {
-        evt.preventDefault(); // Avoids loosing focus from the editable area
-        document.execCommand(props.cmd, false, props.value); // Send the command to the browser
-      }}
-    >
-      {props.name || props.cmd}
-    </Button>
-  );
+const EditButton: React.FC<{name?: string; cmd: string; value?: string}> = (
+	props
+) => {
+	return (
+		<Button
+			key={props.cmd}
+			style={{margin: '3px'}}
+			onMouseDown={(evt) => {
+				evt.preventDefault(); // Avoids loosing focus from the editable area
+				document.execCommand(props.cmd, false, props.value); // Send the command to the browser
+			}}>
+			{props.children || props.name}
+		</Button>
+	);
+};
+
+const BlockquoteSettings = () => {
+	return (
+		<div style={{display: 'inline-block'}}>
+			<EditButton cmd="bold">
+				<Bold />
+			</EditButton>
+			<EditButton cmd="italic" name={'I'}>
+				<Italic />
+			</EditButton>
+			<EditButton cmd="underline" name={'U'}>
+				<Underline />
+			</EditButton>
+			<EditButton cmd="justifyleft" name={'AL'}></EditButton>
+			<EditButton cmd="justifycenter" name={'AC'}></EditButton>
+			<EditButton cmd="justifyright" name={'AR'}></EditButton>
+			<EditButton cmd="insertorderedlist" name={'Ol'}>
+				<OrderedList />
+			</EditButton>
+			<EditButton cmd="insertunorderedlist" name={'UL'}>
+				<UnorderedList />
+			</EditButton>
+			<EditButton cmd="formatblock" value="blockquote" name={'BQ'}>
+				<Quote />
+			</EditButton>
+			<EditButton cmd="createlink" name={'Link'}>
+				<Link />
+			</EditButton>
+		</div>
+	);
 };
 
 type BlockqouteProps = {
-  text?: string;
-  fontSize?: number;
-  textAlign?: string;
+	text?: string;
+	fontSize?: number;
+	textAlign?: string;
 };
 
-
-Blockqoute.craft ={
-  displayName: "Blockquote",
-}
-
+Blockqoute.craft = {
+	displayName: 'Blockquote',
+	related: {
+		settings: BlockquoteSettings,
+	},
+};
 
 export default Blockqoute;
