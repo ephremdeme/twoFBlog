2e261b31facb2c90164daecd1682073152abbafc
Ephrem Demelash <demelashephrem@gmail.com>
2021-03-06 22:33:41 +0300

    🐛 FIX: typos and minor bugs

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,19 +1,38 @@
 import {Editor, Element, Frame} from '@craftjs/core';
-import {Box} from '@material-ui/core';
+import {Box, Grid} from '@material-ui/core';
+import Container from 'components/user/container/container';
+import {Toolbox} from 'layouts/editor';
+import NavBar from 'layouts/editor/appbar';
+import MiniDrawer from 'layouts/editor/drawer';
 import React from 'react';
 import RenderNode from '../components/user/RenderNode';
-import {Text} from '../components/user/text/text';
+import {Text} from '../components/user/text/Text';
 
 const EditorPage = () => {
 	const resolvers = {Text};
 
 	return (
 		<Editor resolver={resolvers} onRender={RenderNode}>
-			<Frame>
-				<Box my={8}>
-					<Element is={Text} id="test" />
-				</Box>
-			</Frame>
+			<NavBar />
+			<Grid container spacing={5}>
+				<Grid item md={1}>
+					<MiniDrawer />
+				</Grid>
+				<Grid item xs={12} md={8}>
+					<Frame>
+						<Box my={10}>
+							<Element is={Text} id="test" />
+
+							<Box my={10} color="red" minHeight="50px">
+								<Element is={Container} canvas>
+									<Element is={Text} id="test12" />
+								</Element>
+							</Box>
+						</Box>
+					</Frame>
+				</Grid>
+				<Grid item xs={12} md={2}></Grid>
+			</Grid>
 		</Editor>
 	);
 };
