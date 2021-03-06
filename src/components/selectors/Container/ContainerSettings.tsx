2ffb697ac3f0f47441b93e9750d38b510bdb10d4
leularia <leulsegedtariku@gmail.com>
2021-03-06 10:29:31 +0300

    ✨ NEW FEATURE: added Container and Resizer

---
+++ src/components/selectors/Container/ContainerSettings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const ContainerSettings = () => {
+	return (
+		<React.Fragment>
+			<h1>Container Component</h1>
+			<p>Settings</p>
+		</React.Fragment>
+	);
+};
--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,17 +1,20 @@
 import {Editor, Element, Frame} from '@craftjs/core';
 import {Box} from '@material-ui/core';
+import {Container} from 'components/selectors/Container';
 import React from 'react';
 import Blockqoute from '../components/user/blockqoute';
 import RenderNode from '../components/user/RenderNode';
 
 const EditorPage = () => {
-	const resolvers = {Blockqoute};
+	const resolvers = {Blockqoute, Container};
 
 	return (
 		<Editor resolver={resolvers} onRender={RenderNode}>
 			<Frame>
 				<Box my={8}>
-					<Element is={Blockqoute} id="test" />
+					<Element is={Container} background={{r: 255, g: 55, b: 55, a: 1}} padding={['10', '10', '10', '10']} >
+						<Element is={Blockqoute} id="test" />
+					</Element>
 				</Box>
 			</Frame>
 		</Editor>
