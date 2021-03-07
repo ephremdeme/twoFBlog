647e976ded7c015e7f766b1be5c0f18d4985365d
Ephrem Demelash <demelashephrem@gmail.com>
2021-03-08 00:38:03 +0300

    👌 IMPROVE: adds customize image to fit

--- src/components/user/image/image.tsx
+++ src/components/user/image/image.tsx
@@ -1,10 +1,11 @@
 import {useNode, UserComponent} from '@craftjs/core';
-import {Button, makeStyles} from '@material-ui/core';
+import {Button, makeStyles, Container as MuiContainer} from '@material-ui/core';
 import React, {useEffect, useState} from 'react';
 import InsertPhotoIcon from '@material-ui/icons/InsertPhoto';
 import {Container} from 'components/selectors/Container';
+import {ImageSettings} from './imageSettings';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles({
 	root: {
 		'& > *': {
 			margin: 0,
@@ -12,6 +13,24 @@ const useStyles = makeStyles((theme) => ({
 	},
 	input: {
 		display: 'none',
+	},
+
+	bestFit: {
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		maxWidth: '100%',
+	},
+	small: {
+		marginLeft: 'auto',
+		marginRight: 'auto',
+		maxWidth: '100%',
+		width: '360px',
+		minWidth: '360px',
+	},
+
+	fullWidth: {
+		maxWidth: '940px',
+		width: `calc(100vw - 40px)`,
 	},
 
 	fill: {
@@ -22,18 +41,27 @@ const useStyles = makeStyles((theme) => ({
 		position: 'relative',
 		margin: 0,
 		padding: 0,
-		'& img': {
-			flexShrink: 0,
-			minWidth: '10%',
-			minHeight: '100%',
-			verticalAlign: 'middle',
-			maxWidth: '100%',
-			height: 'auto',
-		},
 	},
-}));
+	img: {
+		minWidth: '10%',
+		maxWidth: '100%',
+		display: 'block',
+		position: 'relative',
+		overflow: 'hidden',
+	},
+});
 
-export const Image: UserComponent = () => {
+type ImageProp = {
+	bestFit?: boolean;
+	small?: boolean;
+	fullWidth?: boolean;
+};
+
+export const Image: UserComponent<ImageProp> = ({
+	bestFit,
+	small,
+	fullWidth,
+}) => {
 	const {
 		connectors: {connect, drag},
 	} = useNode();
@@ -60,7 +88,22 @@ export const Image: UserComponent = () => {
 	return (
 		<div ref={(ref) => connect(drag(ref))}>
 			<Container className={classes.fill + ' rounded z-depth-2'}>
-				<img src={src === 'null' ? undefined : src} alt={alt} />
+				<div
+					className={
+						small
+							? classes.small
+							: bestFit
+							? classes.bestFit
+							: fullWidth
+							? classes.fullWidth
+							: undefined
+					}>
+					<img
+						src={src === 'null' ? undefined : src}
+						alt={alt}
+						className={classes.img}
+					/>
+				</div>
 			</Container>
 
 			<input
@@ -86,4 +129,63 @@ export const Image: UserComponent = () => {
 
 Image.craft = {
 	displayName: 'ImageUploadDragable',
+	props: {
+		small: false,
+		bestFit: false,
+		fullWidth: false,
+	},
+	related: {
+		settings: ImageSettings,
+	},
+};
+
+export const CoverImage: React.FC<{
+	handleChange: (key: string, value: string) => void;
+}> = ({handleChange}) => {
+	const classes = useStyles();
+	const [{alt, src}, setImg] = useState({
+		src: '',
+		alt: '',
+	});
+
+	const handleFileChange = (e: any) => {
+		if (e.target.files[0]) {
+			handleChange('coverImage', e.target.files[0]);
+			setImg({
+				src: URL.createObjectURL(e.target.files[0]),
+				alt: e.target.files[0].name,
+			});
+		}
+	};
+
+	return (
+		<div className={classes.root}>
+			<MuiContainer className={classes.fill}>
+				<img
+					src={src}
+					alt={alt}
+					className={classes.img}
+					style={{maxWidth: '100%', height: 'auto', width: 'auto'}}
+				/>
+			</MuiContainer>
+
+			<input
+				accept="image/*"
+				className={classes.input}
+				id="icon-button-file"
+				type="file"
+				onChange={handleFileChange}
+			/>
+			<label htmlFor="icon-button-file">
+				<Button
+					variant="outlined"
+					size="medium"
+					aria-label="upload picture"
+					component="span"
+					startIcon={<InsertPhotoIcon />}>
+					Add Cover Image
+				</Button>
+			</label>
+		</div>
+	);
 };
