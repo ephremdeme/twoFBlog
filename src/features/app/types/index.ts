0674709d2eb5661b059e3adcd75618507c4cb8b8
leularia <leulsegedtariku@gmail.com>
2021-03-11 10:48:10 +0300

    🚸 IMPROVE: add seller,shope_admin, blogger roles

--- src/features/app/types/index.ts
+++ src/features/app/types/index.ts
@@ -1,17 +1,17 @@
 export enum UserRole {
 	ADMIN = 'ADMIN',
+	BLOGGER = "BLOGGER",
 	CUSTOMER_SERVICE = 'CUSTOMER_SERVICE',
 	USER = 'USER',
 	GUEST = 'GUEST',
 	SHOPE_ADMIN = 'SHOPE_ADMIN',
-	SHOPE_EMPLOYEE = 'SHOPE_EMPLOYEE'
+	SELLER = 'SELLER'
 }
-
 export interface User {
 	uid: string;
 	role: UserRole;
 	email: string;
 	photo: string;
 	user_name: string;
-	isOnline: string;
+	isOnline: boolean;
 }
