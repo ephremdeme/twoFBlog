0769c1c44bad3d300cdc35d62960943028bd4ca1
leulhabte <leulh63@gmail.com>
2021-03-19 08:03:40 -0700

    firebase config

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,22 +1,22 @@
-// const firebaseConfig = {
-//   apiKey: "AIzaSyARoF96Y6WrrViedbRKWHQAMykf3zlU2Ho",
-//   authDomain: "blog-editor-d6d45.firebaseapp.com",
-//   databaseURL: "https://blog-editor-d6d45-default-rtdb.firebaseio.com",
-//   projectId: "blog-editor-d6d45",
-//   storageBucket: "blog-editor-d6d45.appspot.com",
-//   messagingSenderId: "905032591938",
-//   appId: "1:905032591938:web:fedd049b8c2747379e9bdb",
-//   measurementId: "G-YQMC0M9DV7"
-// }
-
 const firebaseConfig = {
-  apiKey: "AIzaSyA3C5NJM_WGYBEWEHNHEK_-R8-GdNmqZPU",
-  authDomain: "cooomerce.firebaseapp.com",
-  projectId: "cooomerce",
-  storageBucket: "cooomerce.appspot.com",
-  messagingSenderId: "1004065598579",
-  appId: "1:1004065598579:web:4620e8cd73803a1bbe0e06",
-  measurementId: "G-L20B48J0TF"
-};
+  apiKey: "AIzaSyARoF96Y6WrrViedbRKWHQAMykf3zlU2Ho",
+  authDomain: "blog-editor-d6d45.firebaseapp.com",
+  databaseURL: "https://blog-editor-d6d45-default-rtdb.firebaseio.com",
+  projectId: "blog-editor-d6d45",
+  storageBucket: "blog-editor-d6d45.appspot.com",
+  messagingSenderId: "905032591938",
+  appId: "1:905032591938:web:fedd049b8c2747379e9bdb",
+  measurementId: "G-YQMC0M9DV7"
+}
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA3C5NJM_WGYBEWEHNHEK_-R8-GdNmqZPU",
+//   authDomain: "cooomerce.firebaseapp.com",
+//   projectId: "cooomerce",
+//   storageBucket: "cooomerce.appspot.com",
+//   messagingSenderId: "1004065598579",
+//   appId: "1:1004065598579:web:4620e8cd73803a1bbe0e06",
+//   measurementId: "G-L20B48J0TF"
+// };
 
 export default firebaseConfig
