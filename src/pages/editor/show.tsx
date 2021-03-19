81f04a194e00e2ef5c4673a12f81d9a3552296f7
ephremdeme <demelashephrem@gmail.com>
2021-03-19 23:53:14 +0300

    🐛 FIX: Routing issues
    - editor accepts hashes and user data
    
    - show renders correctly with route blogId
    - fix orders of blogs routing

--- src/pages/editor/show.tsx
+++ src/pages/editor/show.tsx
@@ -2,13 +2,15 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 import EditorPage from './editor';
-import {IBlog} from '../../features/editor';
+import {IBlog, selectBlog, setBlog} from '../../features/editor';
 import {useFireDoc} from 'hooks/useFirestore';
+import {useSelector} from 'react-redux';
 
 function ShowBlog() {
 	const {blogId} = useParams<{blogId: string}>();
-	const {data, loading} = useFireDoc<IBlog>('blogs', blogId);
-	console.log('Blog', data);
+	const {data, loading} = useFireDoc<IBlog>('blogs', blogId, setBlog);
+	const blog = useSelector(selectBlog);
+	console.log('Blog', blog);
 
 	if (loading) return <h1>Loading</h1>;
 	return <EditorPage edit={true} blog={data} />;
