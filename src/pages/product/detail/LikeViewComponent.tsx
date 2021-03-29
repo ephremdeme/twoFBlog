529adcf10faf619456b6b298635c6d306894f386
leularia <leulsegedtariku@gmail.com>
2021-03-29 14:11:05 +0300

    👌 IMPROVE: added likes and comments

--- src/pages/product/detail/LikeViewComponent.tsx
+++ src/pages/product/detail/LikeViewComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Box, Button, ButtonGroup } from '@material-ui/core'
+import { Box, Button, ButtonGroup, FormControl, IconButton, Input, InputAdornment, InputLabel } from '@material-ui/core'
 import VisibilityIcon from '@material-ui/icons/Visibility';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import ChatRoundedIcon from '@material-ui/icons/ChatRounded';
@@ -8,6 +8,9 @@ import { getCollection } from 'app/hooks';
 import { collectionData } from 'rxfire/firestore';
 import { useSelector } from 'react-redux';
 import { RootState } from 'app/store';
+import ChatIcon from '@material-ui/icons/Chat';
+import SendIcon from '@material-ui/icons/Send';
+import Comments from './Comments'
 
 interface IProps {
   id: string;
@@ -17,45 +20,100 @@ const LikeViewComponent: React.FC<IProps> = ({ id }) => {
   const userId = useSelector((state: RootState) => state.auth.uid);
   const [viewsCount, setViewsCount] = useState(0);
   const [likesCount, setLikesCount] = useState(0);
-
+  const [liked, setLiked] = useState(false);
+  const [viewed, setViwed] = useState(false);
+  const [commented, setCommented] = useState(false);
+  const [comment, setComment] = useState("");
+  const [commentCount, setCommentCount] = useState(0);
+  
   useEffect(() => {
-    const views$ = getCollection(PDB.PRODCUTS).doc(id).collection('views');
-    getCollection(PDB.PRODCUTS).doc(id).collection('views').add({
-      id: userId
-    })
-    setTimeout(() => setViewsCount(viewsCount+1), 2000)
 
-    collectionData(views$)
-    .subscribe((data) => {
-      setLikesCount(data.length)
+    if (getCollection(PDB.PRODCUTS).doc(id).collection('views').doc(userId).get()) setViwed(true);
+    if (getCollection(PDB.PRODCUTS).doc(id).collection('views').doc(userId).get()) setLiked(true);
+    if (getCollection(PDB.PRODCUTS).doc(id).collection('comments').doc(userId).get()) setCommented(true);
+
+    getCollection(PDB.PRODCUTS).doc(id).collection('views').doc(userId).set({
+      liked: true
+    });
+
+    const views$ = getCollection(PDB.PRODCUTS).doc(id).collection('views');
+    views$.onSnapshot((data) => {
+      if (data) {
+        const viewsCountRef = data.docs.map((doc: any) => doc.data()).length;
+        setViewsCount(viewsCountRef)
+      }
     })
-    const like$ = getCollection(PDB.PRODCUTS).doc(id).collection('likes');
-    collectionData(like$)
-    .subscribe((data) => {
-      setLikesCount(data.length)
+
+    getCollection(PDB.PRODCUTS).doc(id).collection('comments').onSnapshot((commentRef: any) => {
+      const comments = commentRef.docs.map((doc: any) => doc.data())
+      console.log('Comments: ', comments)
+      setCommentCount(comments.length)
+    });
+
+    const likes$ = getCollection(PDB.PRODCUTS).doc(id).collection('likes');
+    likes$.onSnapshot((data) => {
+      if (data) {
+        const likesCountRef = data.docs.map((doc: any) => doc.data()).length;
+        setLikesCount(likesCountRef);
+      }
     })
+
   }, []);
 
   const handleLike = () => {
-    alert('k')
-    getCollection(PDB.PRODCUTS).doc(id).collection('likes').add({
-      id: userId
+    if (liked) {
+      getCollection(PDB.PRODCUTS).doc(id).collection('likes').doc(userId).delete();
+      setLiked(false)
+    } else {
+      getCollection(PDB.PRODCUTS).doc(id).collection('likes').doc(userId).set({
+        liked: true
+      })
+      setLiked(true)
+    }
+  }
+
+  const handleComment = () => {
+    getCollection(PDB.PRODCUTS).doc(id).collection('comments').add({
+      comment: comment,
+      uid: userId
     })
+    setComment("");
   }
 
   return (
-    <Box>
+    <Box display="flex" flexDirection="column" width="100%">
       <ButtonGroup
         size="small"
         aria-label="small outlined button group">
-        <Button startIcon={<VisibilityIcon />}>{viewsCount}</Button>
-        <Button startIcon={<FavoriteIcon />} onClick={() => handleLike} >
-          {/* onClick={handleLike}> */}
-          {/* {like} */}
+        <Button startIcon={<VisibilityIcon />} color={viewed ? "primary" : "default"}>{viewsCount}</Button>
+        <Button startIcon={<FavoriteIcon />} onClick={handleLike} color={viewed ? "primary" : "default"} >
           {likesCount}
         </Button>
-        {/* <Button startIcon={<ChatRoundedIcon />}>34</Button> */}
+        <Button startIcon={<ChatRoundedIcon />}>{commentCount}</Button>
       </ButtonGroup>
+      <Box mt={3} display="flex" py>
+        <FormControl>
+          <InputLabel htmlFor="input-with-icon-adornment">With a start adornment</InputLabel>
+          <Input
+            id="input-with-icon-adornment"
+            value={comment}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setComment(e.target.value)
+            }}
+            startAdornment={
+              <InputAdornment position="start">
+                <ChatIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <IconButton onClick={handleComment}>
+          <SendIcon />
+        </IconButton>
+      </Box>
+      <Box>
+        <Comments  id={id} />
+      </Box>
     </Box>
   )
 }
