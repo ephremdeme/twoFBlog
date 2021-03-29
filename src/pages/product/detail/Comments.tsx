bcc996004b0216e42444aef0c7bbc208c65af421
leularia <leulsegedtariku@gmail.com>
2021-03-29 14:50:47 +0300

    👌 IMPROVE: added coment functionality

--- src/pages/product/detail/Comments.tsx
+++ src/pages/product/detail/Comments.tsx
@@ -11,6 +11,7 @@ import { getCollection } from 'app/hooks';
 import { PDB } from 'features/product/init';
 import LoadingOnly from 'components/shared/LoadingOnly';
 import { Box } from '@material-ui/core';
+import { IAuthor } from 'features/editor';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -36,7 +37,52 @@ const Comments: React.FC<IProps> = ({ id }) => {
   useEffect(() => {
 
     const fetchUserComment = async () => {
-      getCollection(PDB.PRODCUTS).doc(id).collection('comments').onSnapshot((commentRef: any) => {
+      getCollection(PDB.PRODCUTS).doc(id).collection('comments').onSnapshot((commentRef) => {
+        let commentDatas: any[] = [];
+
+        let authors: any[] = [];
+
+        commentRef.forEach(doc=>{
+
+          const data = doc.data();
+          const comm: any = null;
+
+          // comm.comment = data.comment;
+
+          commentDatas.push({
+            ...data,
+            id: doc.id,
+            uid: data.uid.id
+          })
+
+
+          let refData = data.uid.get();
+          authors.push(refData);
+        })
+
+          Promise.all(authors).then((values: any) => {
+            interface IAuther {
+              [index: string]: {};
+            }
+    
+            let authorArr: IAuther = {};
+            values.map((value: any) => {
+              authorArr[value.id] = {
+                uid: value.id,
+                user_name: value.data().user_name,
+                photo: value.data().photo,
+              };
+              
+              return authorArr;
+            });
+    
+            commentDatas = commentDatas.map((comment, index) => ({
+              ...comment,
+              author: authorArr[comment.uid] as IAuthor,
+            }));
+
+            setComments(commentDatas)
+        })
         
         // const comments = commentRef.docs.map((doc: any) => {
         //   getCollection('users').doc(doc.data().uid).get().then(((dataUser: any) => {
@@ -62,10 +108,10 @@ const Comments: React.FC<IProps> = ({ id }) => {
           <List className={classes.root}>
             <ListItem alignItems="flex-start">
               <ListItemAvatar>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+                <Avatar alt="Remy Sharp" src={comment.author.photo}/>
               </ListItemAvatar>
               <ListItemText
-                primary="User"
+                primary={comment.author.user_name}
                 secondary={comment.comment}
               />
             </ListItem>
